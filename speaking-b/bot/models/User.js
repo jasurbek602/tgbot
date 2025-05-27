9ef58f152aec6cca75a59c6ffb0f9822cb118caf
jasurbek602 <115354796+jasurbek602@users.noreply.github.com>
2025-05-27 18:55:43 +0500

    Delete speaking-b directory

--- speaking-b/bot/models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  telegramId: { type: Number, required: true, unique: true },
-  phone: String
-});
-const User = mongoose.model('User', userSchema);
-module.exports = { User };
